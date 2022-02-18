c42f928b81aa3702f31e9321b6bcb6b10df38e86
Andrew Wright <wrightak@gmail.com>
2022-02-18 13:52:24 +0900

    Dummy and stub

---
+++ src/missile/ExpiredLaunchCodeStub.java
@@ -0,0 +1,8 @@
+package missile;
+
+public class ExpiredLaunchCodeStub implements LaunchCode {
+    @Override
+    public boolean isExpired() {
+        return true;
+    }
+}
--- src/missile/MissileLauncherTest.java
+++ src/missile/MissileLauncherTest.java
@@ -2,5 +2,11 @@ package missile;
 
 import org.junit.jupiter.api.Test;
 
+import static missile.MissileLauncher.launchMissile;
+
 class MissileLauncherTest {
+    @Test
+    public void givenExpiredLaunchCodes_MissileIsNotLaunched() {
+        launchMissile(new DummyMissile(), new ExpiredLaunchCodeStub());
+    }
 }
